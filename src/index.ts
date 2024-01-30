c56cd210f79b5c120d7a0ec0de019cde6dc34a72
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-01-29 21:40:42 -0500

    feat: create example api with email-password provider

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { createUser, signinUser, signoutUser } from './example/email-password';
+import { Firegrace } from './utils/firegrace';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const firegrace = new Firegrace();
+
+app.use(express.json());
+
+app.post('/api/ex/auth/create', async (req: Request, res: Response) => {
+    const email: string = req.body.email || '';
+    const password: string = req.body.password || '';
+
+    const response = await createUser(firegrace.authentication!, email, password);
+
+    res.status(response.code).send(response.message);
+});
+
+app.post('/api/ex/auth/in', async (req: Request, res: Response) => {
+    const email: string = req.body.email || '';
+    const password: string = req.body.password || '';
+
+    const response = await signinUser(firegrace.authentication!, email, password);
+
+    res.status(response.code).send(response.message);
+});
+
+app.post('/api/ex/auth/out', async (req: Request, res: Response) => {
+    await signinUser(firegrace.authentication!, 'foo@bar.com', 'password');
+    await signoutUser(firegrace.authentication!);
+
+    res.status(200).send();
+});
 
 app.listen(port, () => {
     console.log(`[server] grace-authentication-api is available at http://localhost:${port}/`);
