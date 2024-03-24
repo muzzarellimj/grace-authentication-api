eff591df617557e0f22d8307b72be645c57b3934
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-24 18:36:48 -0400

    feat: create Firestore service module, findOne and storeOne functions

---
+++ src/services/firestore.service.ts
@@ -0,0 +1,43 @@
+import {
+    DocumentData,
+    WhereFilterOp,
+    collection,
+    doc,
+    getDocs,
+    getFirestore,
+    query,
+    setDoc,
+    where,
+} from 'firebase/firestore';
+
+export const enum FirestorePath {
+    SESSION = 'sessions',
+    USER = 'users',
+}
+
+export class FirestoreService {
+    static async findOne(
+        path: FirestorePath,
+        key: string,
+        operator: WhereFilterOp,
+        value: any
+    ): Promise<DocumentData | null> {
+        const queryStatement = query(collection(getFirestore(), path), where(key, operator, value));
+        const snapshot = await getDocs(queryStatement);
+
+        if (snapshot.empty) {
+            return null;
+        }
+
+        return snapshot.docs[0].data();
+    }
+
+    static async storeOne(path: FirestorePath, document: any): Promise<void> {
+        const documentReference = doc(collection(getFirestore(), path));
+
+        document.id = documentReference.id;
+        document.createdAt = Date.now();
+
+        await setDoc(documentReference, document);
+    }
+}
