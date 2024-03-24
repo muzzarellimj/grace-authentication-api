2fbd5c172f5e56a019e93fddaf90f0d7693c03bc
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-24 00:07:45 -0400

    refactor: migrate Firebase app initialization to index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { initialiseFirebase } from './utils/firebase';
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import { initializeApp } from "firebase/app";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-initialiseFirebase();
+initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+});
 
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`grace-authentication-api is available at http://localhost:${port}/`);
+    console.log(
+        `grace-authentication-api is available at http://localhost:${port}/`
+    );
 });
