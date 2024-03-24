bf305584accf7bfd0890c1e551b7fed760c849f7
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-23 22:58:51 -0400

    chore: remove example API routes and handlers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { createUser, signinUser, signoutUser } from './example/email-password';
 import { Firegrace } from './utils/firegrace';
 
 dotenv.config();
@@ -11,31 +10,6 @@ const firegrace = new Firegrace();
 
 app.use(express.json());
 
-app.post('/api/ex/auth/create', async (req: Request, res: Response) => {
-    const email: string = req.body.email || '';
-    const password: string = req.body.password || '';
-
-    const response = await createUser(firegrace.authentication!, email, password);
-
-    res.status(response.code).send(response.message);
-});
-
-app.post('/api/ex/auth/in', async (req: Request, res: Response) => {
-    const email: string = req.body.email || '';
-    const password: string = req.body.password || '';
-
-    const response = await signinUser(firegrace.authentication!, email, password);
-
-    res.status(response.code).send(response.message);
-});
-
-app.post('/api/ex/auth/out', async (req: Request, res: Response) => {
-    await signinUser(firegrace.authentication!, 'foo@bar.com', 'password');
-    await signoutUser(firegrace.authentication!);
-
-    res.status(200).send();
-});
-
 app.listen(port, () => {
-    console.log(`[server] grace-authentication-api is available at http://localhost:${port}/`);
+    console.log(`grace-authentication-api is available at http://localhost:${port}/`);
 });
