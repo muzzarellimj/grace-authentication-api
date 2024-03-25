73ad40c6625949a1a81bf417f017188cb62471d6
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-24 23:29:31 -0400

    feat: add DEFAULT_JWT_TTL constant value

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
+// password salt and hash
 export const DEFAULT_SALT_LENGTH: number = 16;
 export const DEFAULT_SALT_ROUND_COUNT: number = 10;
+
+// JWT signing
+export const DEFAULT_JWT_TTL: string = '30d';
