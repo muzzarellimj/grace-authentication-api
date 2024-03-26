a038db7df237ff90ab346d79b2ba66be05156643
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-25 21:12:32 -0400

    refactor: validate user creation args in middleware with Zod package

---
+++ src/middleware/validation.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ZodSchema, z } from 'zod';
+
+const MIN_PASSWORD_LENGTH: number = 8;
+
+const userCreationArgsSchema: ZodSchema = z.object({
+    email: z.string().email({ message: 'Please enter a valid email address.' }),
+    password: z.string().min(MIN_PASSWORD_LENGTH, { message: 'Please enter a password with 8 or more characters.' }),
+    firstName: z.string({
+        required_error: 'Please provide an appropriate first name.',
+    }),
+    lastName: z.string({
+        required_error: 'Please provide an appropriate last name.',
+    }),
+});
+
+export function validateUserCreationArgs(request: Request, response: Response, next: NextFunction) {
+    const validationState = userCreationArgsSchema.safeParse({
+        email: request.body.email,
+        password: request.body.password,
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+    });
+
+    if (!validationState.success) {
+        let message: string =
+            validationState.error.issues[0]?.message ?? 'Oops! We hit a snag. Please try again later.';
+
+        return response.status(StatusCodes.BAD_REQUEST).json({ status: StatusCodes.BAD_REQUEST, message: message });
+    }
+
+    next();
+}
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,27 +1,13 @@
 import bcrypt from 'bcrypt';
 import express, { Request, Response, Router } from 'express';
 import { DEFAULT_SALT_ROUND_COUNT } from '../constants';
+import { validateUserCreationArgs } from '../middleware/validation';
 import { FirestorePath, FirestoreService } from '../services/firestore.service';
-import { ValidationService } from '../services/validation.service';
 
 const router: Router = express.Router();
 
-router.post('/signup', async (request: Request, response: Response) => {
+router.post('/signup', validateUserCreationArgs, async (request: Request, response: Response) => {
     const { email, password, firstName, lastName } = request.body;
-
-    const isEmailValid = ValidationService.validateEmailAddress(email);
-
-    if (!isEmailValid) {
-        return response.status(400).json({ message: 'Please provide a valid email address.' });
-    }
-
-    if (!password) {
-        return response.status(400).json({ message: 'Please provide a valid password.' });
-    }
-
-    if (!firstName || !lastName) {
-        return response.status(400).json({ message: 'Please provide a valid first and last.' });
-    }
 
     const existingUser = await FirestoreService.findOne(FirestorePath.USER, 'email', '==', email.toLowerCase());
 
