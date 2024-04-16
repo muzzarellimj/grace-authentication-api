058ed51a892e642ed2178a41c8fb148e3b17e8be
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-04-15 23:06:37 -0400

    build: configure jest with typescript support, install testing dependencies

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const configuration = {
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+};
+
+export default configuration;
